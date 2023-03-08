adc4ec8bc7fb0a2364442fd8e731d8d59a13e5de
zhiying_mar <zhiying_mar@163.com>
2023-03-08 11:44:28 +0800

    feat：合并前端和后端

--- background/app.js
+++ background/app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+// __dirname,代表当前文件路径
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -17,9 +18,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const request=axios.get('/users');
+  request.then(res=>{
+    console.log(res.data);
+  })
   return (
     <div className="App">
       <header className="App-header">
